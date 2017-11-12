89e421226eeff76421a868ba0ce0285a0435518d
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 06:15:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function upperCaseCustomerName()
 {
      customerName = customerName.toUpperCase();
 }// Write your solution in this file!
+
+function setBestCustomer()
+{
+
+}
+
+function changeLeastFavoriteCustomer()
+{
+  
+}
+
+function attemptTwoFavoriteCustomers()
+{
+  
+}
