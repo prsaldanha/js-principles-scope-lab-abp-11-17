4a21c518e58a70f0209f7e4d0a6adecae2587025
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-12 06:28:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName()
+{
+     customerName = customerName.toUpperCase();
+}// Write your solution in this file!
+
+function setBestCustomer()
+{
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer()
+{
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Prem';
+function changeLeastFavoriteCustomer()
+{
+  leastFavoriteCustomer = 'Ryan';
+}
+
+let favoriteCustomer = 'Prem';
+function attemptTwoFavoriteCustomers()
+{
+ let favoriteCustomer = 'Rachel';
+ throw new SyntaxError;
+}
